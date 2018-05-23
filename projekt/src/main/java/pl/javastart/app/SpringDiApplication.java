976729f674dbdde7d7595783b6275845be944c70
ibm <ibm@Lenovo-PC.home>
2018-05-24 00:55:12 +0200

    cm3

--- projekt/src/main/java/pl/javastart/app/SpringDiApplication.java
+++ projekt/src/main/java/pl/javastart/app/SpringDiApplication.java
@@ -1,5 +1,21 @@
 package pl.javastart.app;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import projekt.MessagePrinter;
+
 public class SpringDiApplication {
+	
+	public static void main(String[] args) {
+	
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+		MessagePrinter printer = ctx.getBean(MessagePrinter.class);
+		printer.printMessage();
+		ctx.close();
+		
+	
+	}
+	
+	
 
 }
